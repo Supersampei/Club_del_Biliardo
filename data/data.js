3328fe41a669929d25c7c350dd48de466beefa38
Supersampei <megasampei@gmail.com>
2025-09-29 10:44:14 +0200

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,35 @@
+{
+  "giocatori": [
+    {
+      "nome": "Eugenio",
+      "nickname": "Il Maestro",
+      "email": "eugenio@email.it",
+      "foto": ""
+    },
+    {
+      "nome": "Lorenzo",
+      "nickname": "The Shark",
+      "email": "",
+      "foto": ""
+    }
+  ],
+  "luoghi": [
+    {
+      "nome": "Sala Iguaçu",
+      "indirizzo": "Via Roma 10",
+      "maps": "https://goo.gl/maps/xxxx",
+      "foto": ""
+    }
+  ],
+  "partite": [
+    {
+      "tipo": "8-15",
+      "modalita": "singolo",
+      "giocatori": [0, 1],
+      "luogo": 0,
+      "dataPartita": "2025-09-29",
+      "vittorie": [2, 3],
+      "dettagli": ["V", "P", "P", "P", "P"]
+    }
+  ]
+}
